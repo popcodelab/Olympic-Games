88508884f2f17bab35d9b389209678688580308e
POP's Code Lab <pop.code.lab13@gmail.com>
2024-04-07 22:09:46 +0700

    add missing comments in the footer component

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
+/**
+ * Represents the footer component of the application.
+ *
+ * @component
+ * @selector app-footer
+ * @templateUrl ./footer.component.html
+ * @styleUrls ./footer.component.scss
+ */
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss']
 })
-export class FooterComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+export class FooterComponent{
 
 }
